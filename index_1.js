a49b764a9e78ece78643443b9b3b10cb059194e4
Benjamin De-graft-Andoh <degraftandohbenjamin@gmail.com>
2023-07-11 22:21:53 +0000

    Enyata 1st Javascript Assignment

---
+++ index_1.js
@@ -0,0 +1,7 @@
+let length = 5;
+let width = 3;
+let area = length * width;
+width += 2;
+let perimeter = 2 * (length + width);
+console.log(`area: ${area}`);
+console.log(`perimeter: ${perimeter}`);
